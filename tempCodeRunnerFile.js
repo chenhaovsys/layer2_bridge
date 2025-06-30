98f2299ea181b2089cea362ea541a7e1f8de9d11
chenhaovsys <chenhao.lee@v.systems>
2025-06-30 14:30:40 +0800

    Removed tempcoderunner

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-await main(
-//     "mongodb://localhost:27017",
-//     env.ACC1_SEED_VSYS,
-//     env.ACNT1ADDR_VSYS,
-//     env.ACNT1ADDR_ETH,
-//     env.PRIVATEKEY_ACC1_ETH,
-//     "testnet");
